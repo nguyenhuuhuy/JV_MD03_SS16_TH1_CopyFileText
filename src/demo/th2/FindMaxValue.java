ea0762c9064919e085bb90c9a5a74572460d0173
nguyenhuuhuy <huy.cb96@gmail.com>
2023-03-28 21:06:10 +0700

    bt

---
+++ src/demo/th2/FindMaxValue.java
@@ -0,0 +1,24 @@
+package demo.th2;
+
+import java.util.List;
+
+public class FindMaxValue {
+    public static int findMax(List<Integer> numbers){
+        int max = numbers.get(0);
+        for (int i = 0; i < numbers.size(); i++) {
+            if (max<numbers.get(i)){
+            max = numbers.get(i);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        String PATH_NUMBER = "src/demo/th2/number.txt";
+        String PATH_RESULT = "src/demo/th2/result.txt";
+        List<Integer> numbers = readAndWriteFile.readFile(PATH_NUMBER);
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile(PATH_RESULT,maxValue);
+    }
+}
